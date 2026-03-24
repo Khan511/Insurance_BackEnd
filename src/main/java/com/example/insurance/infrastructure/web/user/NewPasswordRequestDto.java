263c2fb085aeaa10a34fec93cc62c51ee277e565
Khan511 <najeebullahkhan511@yahoo.com>
2026-03-24 11:24:05 +0100

    password length chagne to 4.

--- src/main/java/com/example/insurance/infrastructure/web/user/NewPasswordRequestDto.java
+++ src/main/java/com/example/insurance/infrastructure/web/user/NewPasswordRequestDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 public record NewPasswordRequestDto(
                 @NotBlank(message = "Token is required") String token,
 
-                @NotBlank(message = "Password is required") @Size(min = 4, message = "Password must be at least 4 characters long") @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must contain at least one digit, one lowercase, one uppercase letter, one special character and no whitespace") String newPassword,
+                @NotBlank(message = "Password is required") @Size(min = 4, message = "Password must be at least 4 characters long") @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$", message = "Password must contain at least one digit, one lowercase, one uppercase letter, one special character and no whitespace") String newPassword,
 
                 @NotBlank(message = "Confirm password is required") String confirmPassword
 
